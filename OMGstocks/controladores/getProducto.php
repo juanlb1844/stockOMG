5b7c920127a3b49415339799b6bb72e895924af0
Juan <juanlb1844@gmail.com>
2018-05-11 09:09:58 -0500

    cambiando consultas tomando en cuenta [ flag_status ]

--- OMGstocks/controladores/getProducto.php
+++ OMGstocks/controladores/getProducto.php
@@ -3,22 +3,9 @@
 
  		$db = new db(); 
  		$idProd = $_POST['idProd'];  
-		$query = "SELECT * FROM ( 
-			(SELECT varchar_value AS value_attr, name_attribute AS type_attr, product_entity_id AS ID 
-						  FROM entity_value_varchar AS A1) 
-			 UNION 
-			 ( SELECT picture.main_image_url AS value_attr, 'main_img' AS type_attr, entity.id_entity AS ID 
-				FROM product_entity AS entity
-					LEFT JOIN product_pictures AS picture  
-					ON entity.id_picture = picture.idproduct_pictures)
-			 UNION 
-			(SELECT text_value AS value_attr, name_attribute AS type_attr, product_entity_id AS ID 
-						  FROM entity_value_text AS A2)
-			 UNION 
-			(SELECT DIS.distributor_name AS value_attr, 'distributor' AS type_attr, E.id_entity AS ID FROM 
-						   product_entity AS E INNER JOIN distributor AS DIS
-						   ON E.id_distributor = DIS.id_distributor) 
-				) AS S1 WHERE ID = $idProd"; 
+ 		$idProd = intval($idProd); 
+		$query = "CALL getProductById($idProd)"; 
+
 			if ($resultado = mysqli_query($db, $query)) {
 		    
     		if ($resultado->num_rows > 0) {
@@ -26,7 +13,8 @@
 			    	$rows[] = $row;
 			    }
 			} else {
-			    echo "0 results";
+			    echo "sin resultados";
+			    echo $query; 
 			}
 			echo json_encode($rows); 
    		}
